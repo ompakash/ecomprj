dff1d4727c182b00a1439088a917d2aa53a67fb0
ompakash <opytc2@gmail.com>
2023-05-23 09:48:27 +0530

    custom admin using jazzmin

--- ecomprj/settings.py
+++ ecomprj/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -132,3 +134,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#jazzmin setup
+JAZZMIN_SETTINGS = {
+    "site_header": "Om Shop",  
+    "site_brand": "Om Shop",
+    "site_logo": "assets/imgs/theme/loading.gif",
+    "copyright": "om-shop.com",
+}
+
+
