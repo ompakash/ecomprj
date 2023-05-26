6fae5ca86143b6cef53d18e79c3d43db314137c7
ompakash <opytc2@gmail.com>
2023-05-26 22:47:06 +0530

    signup page created

--- ecomprj/urls.py
+++ ecomprj/urls.py
@@ -20,9 +20,11 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("core.urls")),
+    path("user/",include("userauths.urls")),
 ]
 
 if settings.DEBUG:
--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,logout,authenticate
+from .forms import UserRegisterForm
+from django.contrib import messages
 
 # Create your views here.
+
+def register_view(request):
+    form = UserRegisterForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST or None)
+        if form.is_valid():
+            new_user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Hey {username}, Your account has been created successfully')
+            new_user = authenticate(username = form.cleaned_data['email'], password = form.cleaned_data['password1'])
+            login(request,new_user)
+            print('user created')
+            return redirect("core:index")
+            
+    else:
+        print('User can not be register')
+    context = {
+        'form':form
+    }
+
+    return render(request,'userauths/sign-up.html',context)
